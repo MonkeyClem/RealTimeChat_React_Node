9d28b43ac968d35057237db24cd9f563725c74f0
Clement Jeulin <clement93jeulin@gmail.com>
2023-04-27 22:40:19 +0200

    Bug Node.JS : Routes en ERROR 404

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/*First, we import express, which run the http server
+
+Tout d'abord, nous importons express, qui nous sert à faire fonctionner notre serveur HTTP
+ainsi que CORS, afin que le serveur puisse être depuis n'importe quelle origine
+
+https://blog.chatengine.io/fullstack-chat/nodejs-reactjs
+*/
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
+
 
 const app = express();
 app.use(express.json());
@@ -7,7 +17,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username : username, secret: username, first_name:username},
+        {headers : {"private-key" : "d9229aa8-aa87-4bc7-991e-ca27a90f6642"} }
+        )
+        return res.status(r.status).json(r.data)
+   }catch(e){
+        return res.status(e.response.status).json(e.response.data)
+        } 
+    });
 
 app.listen(3001);
